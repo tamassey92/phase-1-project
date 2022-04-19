bf8c6e1c3c71949b72271a022db1953208d9eac2
Samantha Crespo <samkcrespo@gmail.com>
2022-04-19 12:26:36 -0700

    data rendered

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
   //We need our own db.json to post the comments (so they persist) - Created 4.18.22
   fetch('https://api.imgflip.com/get_memes')
   .then(res => res.json())
-  .then(json => console.log((json.data.memes)));
+  .then(json => getData(json.data.memes.slice(1,11)));
+
+
+//TO-DO 
+//Get data (6-10 memes to display on page) - done 4.19.22
+//Each Meme should have its own like button & form submit(caption!)
+//Persist added comments/captions by using POST method to the db.json file we created
+ 
 
 
  //Use array iteration to get data from our API onto our page. The data should have the meme image/url only. 
+function getData(memes) {
+   memes.forEach((meme)=> {
+const image = document.createElement('img');
+ const container = document.querySelector('#meme-container')
+   image.src = meme.url;
+   container.append(image);
+   })
+
+
+
+}
+
+
+
+
 
 
 //Like button should increment count by 1 each time it is clicked - Partially Done 4.18.22 (Will need to ensure this works for each meme on the page)
+
   const likeBtn = document.querySelector('button')
   likeBtn.addEventListener('click', () => {
 const likes = document.getElementById('likes')
@@ -16,7 +39,7 @@ const count = parseInt(likes.innerText)
 likes.innerText = count + 1 + ' likes';
 })
 
-//Form Submit should append new comment/caption to the ul underneath the specified meme - Partially Done 4.18.22(Will need to ensure this works for each meme on the page)
+// //Form Submit should append new comment/caption to the ul underneath the specified meme - Partially Done 4.18.22(Will need to ensure this works for each meme on the page)
 const form = document.querySelector('#caption-form')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
